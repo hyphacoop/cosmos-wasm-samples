ff6f60a251b17bca2765e18e423bf94a89cefffd
violet <violet@hypha.coop>
2025-09-03 13:56:52 -0400

    Fix the wasm scripts to not redefine $HOME env var

--- bitmap-pay/test-bitmap-pay.sh
+++ bitmap-pay/test-bitmap-pay.sh
@@ -3,7 +3,7 @@
 ########  CHAIN CONFIG ########
 WALLET=cosmos*
 CHAIN_BINARY=gaiad
-HOME=~/.gaia
+CHAIN_HOME=~/.gaia
 CHAIN_DENOM=uatom
 NODE=http://localhost:26657
 CHAIN_ID=testnet
@@ -16,7 +16,7 @@ RECIPIENT=cosmos*
 FILENAME=bitmap-pay/artifacts/bitmap_pay.wasm # cosmwasm/optimizer result
 
 echo "> Storing the contract on chain"
-tx_hash=$($CHAIN_BINARY tx wasm store $FILENAME --home $HOME --from $WALLET --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.txhash')
+tx_hash=$($CHAIN_BINARY tx wasm store $FILENAME --home $CHAIN_HOME --from $WALLET --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.txhash')
 sleep $COMMIT_TIMEOUT
 echo "> Querying the hash for the code ID"
 code_id=$($CHAIN_BINARY query tx $tx_hash -o json --node $NODE | jq -r '.events[] | select(.type=="store_code").attributes[] | select(.key=="code_id").value')
@@ -33,7 +33,7 @@ instantiate_json=$(jq -n \
   --argjson update_fee_factor 10 \
   --arg fee_denom $CHAIN_DENOM \
   '{"x_size":$xsize,"y_size":$ysize, "recipient": $recipient, "supply_base_fee": $supply_base_fee, "supply_fee_factor": $supply_fee_factor, "update_base_fee": $update_base_fee, "update_fee_factor": $update_fee_factor, "fee_denom": $fee_denom}')
-tx_hash=$($CHAIN_BINARY tx wasm instantiate $code_id "$instantiate_json" --home $HOME --label "bitmap" --no-admin --from $WALLET --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.txhash')
+tx_hash=$($CHAIN_BINARY tx wasm instantiate $code_id "$instantiate_json" --home $CHAIN_HOME --label "bitmap" --no-admin --from $WALLET --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.txhash')
 sleep $COMMIT_TIMEOUT
 echo "> Querying the hash for the contract address"
 contract=$($CHAIN_BINARY query tx $tx_hash -o json --node $NODE | jq -r '.events[] | select(.type=="instantiate").attributes[] | select(.key=="_contract_address").value')
@@ -54,7 +54,7 @@ instantiate_json=$(jq -n \
   --argjson update_fee_factor 10 \
   --arg fee_denom $CHAIN_DENOM \
   '{"x_size":$xsize,"y_size":$ysize, "z_values":$zvalues, "recipient": $recipient, "supply_base_fee": $supply_base_fee, "supply_fee_factor": $supply_fee_factor, "update_base_fee": $update_base_fee, "update_fee_factor": $update_fee_factor, "fee_denom": $fee_denom}')
-tx_hash=$($CHAIN_BINARY tx wasm instantiate $code_id "$instantiate_json" --home $HOME --label "bitmap" --no-admin --from $WALLET --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.txhash')
+tx_hash=$($CHAIN_BINARY tx wasm instantiate $code_id "$instantiate_json" --home $CHAIN_HOME --label "bitmap" --no-admin --from $WALLET --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.txhash')
 sleep $COMMIT_TIMEOUT
 echo "> Querying the hash for the contract address"
 contract=$($CHAIN_BINARY query tx $tx_hash -o json --node $NODE | jq -r '.events[] | select(.type=="instantiate").attributes[] | select(.key=="_contract_address").value')
@@ -78,14 +78,14 @@ echo "> Cost at (0,2): $cost"
 
 cost=$($CHAIN_BINARY q wasm contract-state smart $contract '{"get_cost":{"x":0,"y":0}}' -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.data.cost')
 echo "> Executing the contract (unhappy path: bad index)"
-$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":200,"y":0,"z":"0011AA"}}'  --home $HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
+$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":200,"y":0,"z":"0011AA"}}'  --home $CHAIN_HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
 
 echo "> Executing the contract (unhappy path: insufficient funds)"
-$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":0,"z":"0011AA"}}'  --home $HOME --from $WALLET --amount 10uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
+$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":0,"z":"0011AA"}}'  --home $CHAIN_HOME --from $WALLET --amount 10uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
 
 echo "> Setting (0,0) for the first time"
 cost=$($CHAIN_BINARY q wasm contract-state smart $contract '{"get_cost":{"x":0,"y":0}}' -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.data.cost')
-$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":0,"z":"0011AA"}}' --home $HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
+$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":0,"z":"0011AA"}}' --home $CHAIN_HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
 sleep $COMMIT_TIMEOUT
 $CHAIN_BINARY q bank balances $RECIPIENT -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.balances'
 
@@ -101,7 +101,7 @@ echo "> Cost at (0,2): $cost"
 
 echo "> Setting (0,0) for the second time"
 cost=$($CHAIN_BINARY q wasm contract-state smart $contract '{"get_cost":{"x":0,"y":0}}' -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.data.cost')
-$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":0,"z":"0033CC"}}' --home $HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
+$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":0,"z":"0033CC"}}' --home $CHAIN_HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
 sleep $COMMIT_TIMEOUT
 $CHAIN_BINARY q bank balances $RECIPIENT -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.balances'
 
@@ -117,7 +117,7 @@ echo "> Cost at (0,2): $cost"
 
 echo "> Setting (0,0) for the third time"
 cost=$($CHAIN_BINARY q wasm contract-state smart $contract '{"get_cost":{"x":0,"y":0}}' -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.data.cost')
-$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":0,"z":"0033CC"}}' --home $HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
+$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":0,"z":"0033CC"}}' --home $CHAIN_HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
 sleep $COMMIT_TIMEOUT
 $CHAIN_BINARY q bank balances $RECIPIENT -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.balances'
 
@@ -133,7 +133,7 @@ echo "> Cost at (0,2): $cost"
 
 echo "> Setting (0,1) for the first time"
 cost=$($CHAIN_BINARY q wasm contract-state smart $contract '{"get_cost":{"x":0,"y":1}}' -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.data.cost')
-$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":1,"z":"EE1122"}}' --home $HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
+$CHAIN_BINARY tx wasm execute $contract '{"set":{"x":0,"y":1,"z":"EE1122"}}' --home $CHAIN_HOME --from $WALLET --amount ${cost}uatom --gas auto --gas-adjustment 3 --gas-prices $GAS_PRICE -y -o json --node $NODE --chain-id $CHAIN_ID
 sleep $COMMIT_TIMEOUT
 $CHAIN_BINARY q bank balances $RECIPIENT -o json --node $NODE --chain-id $CHAIN_ID | jq -r '.balances'
 
